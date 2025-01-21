0411a7159a13aacce2ca9afcf22c80f2ce370874
stpaes <steefaaniepr@outlook.com>
2025-01-21 18:55:47 -0300

    create tarefa class and project finish

---
+++ tarefas_calculo_medio/src/br/com/stpaes/domain/Tarefa.java
@@ -0,0 +1,13 @@
+package br.com.stpaes.domain;
+
+public class Tarefa {
+
+    public static void main(String[] args) {
+
+        int nota1 = 3, nota2 = 8, nota3 = 9, nota4 = 6, media;
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println(media);
+    }
+
+}
