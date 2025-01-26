2bcc795db682e415f925bb2bc1d675344f6ceb21
stpaes <steefaaniepr@outlook.com>
2025-01-26 19:10:36 -0300

    create in logic the switch and case

---
+++ modulo-controle-fluxo/src/br/com/stpaes/domain/FORWHILE/TabuadaWhile.java
@@ -0,0 +1,27 @@
+package br.com.stpaes.domain.FORWHILE;
+
+import java.util.Scanner;
+
+public class TabuadaWhile {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Deseja gerar a tabuada de algum número? ");
+        String resposta = s.nextLine();
+
+        while (resposta.equalsIgnoreCase("Sim")) {
+        System.out.println("Digite um número para gerar a tabulada: ");
+        int num = s.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println(num + " X " + i + " = " + (num * i));
+        }
+
+        System.out.println("Deseja novamente gerar a tabuada de algum número? ");
+        resposta = s.nextLine();
+
+        }
+
+        System.out.println("Obrigada. ");
+    }
+}
---
+++ modulo-controle-fluxo/src/br/com/stpaes/domain/SWITCHCASE/Frutas.java
@@ -0,0 +1,21 @@
+package br.com.stpaes.domain.SWITCHCASE;
+
+public class Frutas {
+    public static void main(String[] args) {
+        String fruta = "maça";
+
+        switch (fruta) {
+            case "banana":
+                System.out.println("Você escolheu Banana");
+                break;
+            case "maça":
+                System.out.println("Você escolheu Maça");
+                break;
+            case "laranja":
+                System.out.println("Você escolheu Laranja");
+                break;
+            default:
+                System.out.println("Fruta desconhecida");
+        }
+    }
+}
