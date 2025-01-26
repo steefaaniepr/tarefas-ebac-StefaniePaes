c755831bbf64bb838fa0a9f9935c4c3567fca837
stpaes <steefaaniepr@outlook.com>
2025-01-26 18:06:14 -0300

    create in logic the if/else and for

---
+++ modulo-controle-fluxo/src/br/com/stpaes/domain/FORWHILE/TabuadaFor.java
@@ -0,0 +1,16 @@
+package br.com.stpaes.domain.FORWHILE;
+
+import java.util.Scanner;
+
+public class TabuadaFor {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite um valor para a tabuada: ");
+        int num = s.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}
--- modulo-controle-fluxo/src/br/com/stpaes/domain/FORWHILE/forewhile.java
+++ modulo-controle-fluxo/src/br/com/stpaes/domain/FORWHILE/forewhile.java
@@ -1,4 +1,28 @@
 package br.com.stpaes.domain.FORWHILE;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class forewhile {
+
+    public static void main(String[] args) {
+
+        // Exemplo 1
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Linha " + i);
+        }
+
+        // Exemplo 2
+        List<Integer> valores = new ArrayList<>();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Linha " + i);
+            valores.add(i);
+        }
+
+        for (Integer i : valores) {
+            System.out.println("Linha " + i);
+        }
+
+        valores.forEach(i-> System.out.println("Linha " + i));
+    }
 }
