fbcff7ce265c3a92dfaad78c1ad2452fb35e15a9
stpaes <steefaaniepr@outlook.com>
2025-01-30 22:02:13 -0300

    fazendo a tarefa 2

--- tarefa-collection/src/br/com/stpaes/domain/TarefaP2.java
+++ tarefa-collection/src/br/com/stpaes/domain/TarefaP2.java
@@ -1,4 +1,21 @@
 package br.com.stpaes.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class TarefaP2 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite os nomes separados por vírgulas: ");
+        String resultado = s.nextLine();,
+        s.close();
+        String[] resultados = resultado.split("\\s*,\\s*");
+
+        List<String> feminino = new ArrayList<>();
+        List<String> masculino = new ArrayList<>();
+
+    }
 }
