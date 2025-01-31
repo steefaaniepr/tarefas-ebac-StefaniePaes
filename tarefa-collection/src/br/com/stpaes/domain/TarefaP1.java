0b7835c991bac042a45ac5875df2b6276800cfa8
stpaes <steefaaniepr@outlook.com>
2025-01-30 21:49:22 -0300

    Terminando a 1 parte do projeto

--- tarefa-collection/src/br/com/stpaes/domain/TarefaP1.java
+++ tarefa-collection/src/br/com/stpaes/domain/TarefaP1.java
@@ -1,4 +1,26 @@
 package br.com.stpaes.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class TarefaP1 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite os nomes separados por vírgulas: ");
+        String nomes = s.nextLine();
+        s.close();
+
+        String[] nomesJuntos = nomes.split("\\s*,\\s*"); // Assim tira a vírgula e os espaços
+        List<String> nome = new ArrayList<String>(Arrays.asList(nomesJuntos)); // usei para converter diretamente para ArrayList.
+        System.out.println(nome);
+
+        nome.sort(String.CASE_INSENSITIVE_ORDER);
+        System.out.println("-----Nomes Ordenados-----");
+        for (String resultado : nome) {
+            System.out.println(resultado);
+        }
+    }
 }
