23235914ae6b5f19c2fa7cea89f717f338f97090
stpaes <steefaaniepr@outlook.com>
2025-07-16 18:58:25 -0300

    ExemploExceptions

---
+++ ExemploException/src/br/com/stpaes/ExemplosExceptions.java
@@ -0,0 +1,65 @@
+package br.com.stpaes;
+
+public class ExemplosExceptions {
+
+    public static void main(String[] args) {
+        semTratamentoException();
+        comTratamentoException();
+        comTratamentoExceptionComFinally();
+        lancarRuntimeException();
+        System.out.println("Teste");
+    }
+
+    private static void lancarRuntimeException() {
+        try {
+            String frase = null;
+            String novaFrase = null;
+            novaFrase = frase.toUpperCase();
+        } catch (Exception e) {
+            throw new RuntimeException("Parando o sistema");
+        }
+        System.out.println("Teste");
+    }
+
+    private static void comTratamentoException() {
+        String frase = null;
+        String novaFrase = null;
+        try {
+            novaFrase = frase.toUpperCase();
+        } catch (NullPointerException e) {
+            //Tratamento da exceção
+            System.out.println("A frase inicial está nula, para solucionar tal problema, foi lhe atribuida um valor");
+            frase = "Frase vazia";
+            novaFrase = frase.toUpperCase();
+        }
+        System.out.println("Frase antiga: " + frase);
+        System.out.println("Frase antiga: " + novaFrase);
+    }
+
+    private static void comTratamentoExceptionComFinally() {
+        String frase = null;
+        String novaFrase = null;
+        try {
+            novaFrase = frase.toUpperCase();
+        } catch (NullPointerException e) {
+            System.out.println("A frase inicial está nula, para solucionar tal problema, foi lhe atribuido um valor novo");
+            frase = "Frase vazia";
+        } finally {
+            novaFrase = frase.toUpperCase();
+        }
+        System.out.println("Frase antiga: " + frase);
+        System.out.println("Frase nova: " + novaFrase);
+    }
+
+    private static void semTratamentoException() {
+        String frase = null;
+        String novaFrase = null;
+        novaFrase = frase.toUpperCase();
+        System.out.println("Frase antiga: " + frase);
+        System.out.println("Frase nova: " + novaFrase);
+    }
+
+
+
+
+}
---
+++ ExemploException/src/br/com/stpaes/LimiteSaqueException.java
@@ -0,0 +1,7 @@
+package br.com.stpaes;
+
+public class LimiteSaqueException extends RuntimeException {
+    public LimiteSaqueException(String message) {
+        super(message);
+    }
+}
